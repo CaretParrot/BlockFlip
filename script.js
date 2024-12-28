0c2a62e2cd311ceb2ad801b6275159552813cf37
CaretParrot <wopow29@gmail.com>
2024-12-28 12:31:10 -0600

    Implementing hitboxes

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let objects = {
         width: 20,
         height: 20,
         color: "white",
-        x: 200,
+        x: 0,
         y: 150,
         vx: 0,
         vy: 0,
@@ -35,26 +35,28 @@ let objects = {
             painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
         }
     },
-    ground: {
-        x: 0,
-        y: 0,
-        width: canvas.width,
-        height: 20,
-        color: "hsl(0, 0%, 50%)",
-        draw: function () {
-            painter.fillStyle = this.color;
-            painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
-        }
-    },
-    step: {
-        x: 100,
-        y: 50,
-        width: 30,
-        height: 20,
-        color: "hsl(0, 0%, 50%)",
-        draw: function () {
-            painter.fillStyle = this.color;
-            painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
+    platforms: {
+        ground: {
+            x: 0,
+            y: 0,
+            width: canvas.width,
+            height: 20,
+            color: "hsl(0, 0%, 50%)",
+            draw: function () {
+                painter.fillStyle = this.color;
+                painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
+            }
+        },
+        step: {
+            x: 100,
+            y: 30,
+            width: 30,
+            height: 20,
+            color: "hsl(0, 0%, 50%)",
+            draw: function () {
+                painter.fillStyle = this.color;
+                painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
+            }
         }
     }
 }
@@ -63,36 +65,42 @@ let objects = {
 setInterval(function () {
     painter.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (objects.block.y <= objects.ground.height) {
-        objects.block.ay = 0;
-        objects.block.vy = 0;
-        objects.block.y = objects.ground.height;
-        jumping = false;
-    } else {
-        objects.block.ay = -g;
+    
+
+    for (const [key, value] of Object.entries(objects.platforms)) {
+        if (objects.block.x + objects.block.width >= objects.platforms[key].x && objects.block.x <= objects.platforms[key].x && !(objects.block.y >= objects.platforms[key].y + objects.platforms[key].height || objects.block.y + objects.block.height <= objects.platforms[key].y)) {
+            objects.block.ax = 0;
+            objects.block.vx = 0;
+            objects.block.x = objects.platforms[key].x - objects.block.width;
+        }
+
+        if (objects.block.x <= objects.platforms[key].x + objects.platforms[key].width && objects.block.x + objects.block.width >= objects.platforms[key].x + objects.platforms[key].width && !(objects.block.y >= objects.platforms[key].y + objects.platforms[key].height || objects.block.y + objects.block.height <= objects.platforms[key].y)) {
+            objects.block.ax = 0;
+            objects.block.vx = 0;
+            objects.block.x = objects.platforms[key].x + objects.platforms[key].width;
+        }
+
+        if (objects.block.y <= objects.platforms[key].height && objects.block.y + objects.block.height >= objects.platforms[key].height) {
+            objects.block.ay = 0;
+            objects.block.vy = 0;
+            objects.block.y = objects.platforms[key].height;
+            jumping = false;
+        } else {
+            objects.block.ay = -g;
+        }
     }
 
-    if ((objects.block.x + objects.block.width >= objects.step.x && objects.block.x <= objects.step.x)) {
-        objects.block.ax = 0;
-        objects.block.vx = 0;
-        objects.block.x = objects.step.x - objects.block.width;
-    }
 
-    if ((objects.block.x <= objects.step.x + objects.step.width && objects.block.x + objects.block.width >= objects.step.x + objects.step.width)) {
-        objects.block.ax = 0;
-        objects.block.vx = 0;
-        objects.block.x = objects.step.x + objects.step.width;
-    }
+
     if (inputs.left && inputs.right) {
         objects.block.vx = 0;
-
-    }  else if (inputs.left) {
+    } else if (inputs.left) {
         objects.block.ax = -xAcceleration;
     } else if (inputs.right) {
         objects.block.ax = xAcceleration;
-    } 
+    }
 
-    if (!inputs.left && !inputs.right) { 
+    if (!inputs.left && !inputs.right) {
         if (objects.block.vx > 0) {
             objects.block.ax = -friction;
         } else if (objects.block.vx < 0) {
@@ -110,9 +118,9 @@ setInterval(function () {
     objects.block.x += objects.block.vx;
     objects.block.y += objects.block.vy;
 
-    objects.ground.draw();
+    objects.platforms.ground.draw();
     objects.block.draw();
-    objects.step.draw();
+    objects.platforms.step.draw();
 }, 1000 / 60);
 
 onkeydown = function (event) {
@@ -124,7 +132,7 @@ onkeydown = function (event) {
     if (event.key === "ArrowLeft") {
         inputs.left = true;
     }
-    
+
     if (event.key === "ArrowUp" && !jumping) {
         objects.block.y += 1;
         objects.block.vy += jumpPower;
