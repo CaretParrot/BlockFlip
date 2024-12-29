a8607c16fa86b4b72c9543197633378204acaee8
CaretParrot <wopow29@gmail.com>
2024-12-29 12:24:45 -0600

    Almost completed functional hitboxes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let inputs = {
     up: false
 }
 const g = 9.8 / 10;
-const jumpPower = 10;
+const jumpPower = 15;
 const friction = 1;
 const xAcceleration = 0.2;
 
@@ -19,45 +19,30 @@ window.onresize = function (event) {
     canvas.height = document.body.clientHeight - 72;
 }
 
+let GameObject = function (x, y, width, height, color, vx = 0, vy = 0, ax = 0, ay = 0) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.vx = vx;
+    this.vy = vy;
+    this.ax = ax;
+    this.ay = ay;
+    this.draw = function () {
+        painter.fillStyle = this.color;
+        painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
+    }
+}
+
 let objects = {
-    block: {
-        width: 20,
-        height: 20,
-        color: "white",
-        x: 0,
-        y: 150,
-        vx: 0,
-        vy: 0,
-        ax: 0,
-        ay: -9.8,
-        draw: function () {
-            painter.fillStyle = this.color;
-            painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
-        }
-    },
+    block: new GameObject(30, 200, 20, 20, "hsl(0, 0%, 100%)"),
     platforms: {
-        ground: {
-            x: 0,
-            y: 0,
-            width: canvas.width,
-            height: 20,
-            color: "hsl(0, 0%, 50%)",
-            draw: function () {
-                painter.fillStyle = this.color;
-                painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
-            }
-        },
-        step: {
-            x: 100,
-            y: 30,
-            width: 30,
-            height: 20,
-            color: "hsl(0, 0%, 50%)",
-            draw: function () {
-                painter.fillStyle = this.color;
-                painter.fillRect(this.x, canvas.height - this.y - this.height, this.width, this.height);
-            }
-        }
+        ground: new GameObject(0, 0, canvas.width, 20, "hsl(0, 0%, 50%)"),
+        step: new GameObject(100, 20, 100, 20, "hsl(0, 0%, 50%)"),
+        leftWall: new GameObject(0, 0, 20, canvas.height, "hsl(0, 0%, 50%)"),
+        rightWall: new GameObject(canvas.width - 20, 0, 20, canvas.height, "hsl(0, 0%, 50%)"),
+        ceiling: new GameObject(0, canvas.height - 20, canvas.width, 20, "hsl(0, 0%, 50%)")
     }
 }
 
@@ -65,39 +50,42 @@ let objects = {
 setInterval(function () {
     painter.clearRect(0, 0, canvas.width, canvas.height);
 
-    
-
     for (const [key, value] of Object.entries(objects.platforms)) {
-        if (objects.block.x + objects.block.width >= objects.platforms[key].x && objects.block.x <= objects.platforms[key].x && !(objects.block.y >= objects.platforms[key].y + objects.platforms[key].height || objects.block.y + objects.block.height <= objects.platforms[key].y)) {
+        if (objects.block.x + objects.block.width >= objects.platforms[key].x && objects.block.x <= objects.platforms[key].x && objects.block.y < objects.platforms[key].y + objects.platforms[key].height && objects.block.y + objects.block.height > objects.platforms[key].y) {
             objects.block.ax = 0;
             objects.block.vx = 0;
             objects.block.x = objects.platforms[key].x - objects.block.width;
         }
 
-        if (objects.block.x <= objects.platforms[key].x + objects.platforms[key].width && objects.block.x + objects.block.width >= objects.platforms[key].x + objects.platforms[key].width && !(objects.block.y >= objects.platforms[key].y + objects.platforms[key].height || objects.block.y + objects.block.height <= objects.platforms[key].y)) {
+        if (objects.block.x <= objects.platforms[key].x + objects.platforms[key].width && objects.block.x + objects.block.width >= objects.platforms[key].x + objects.platforms[key].width && objects.block.y < objects.platforms[key].y + objects.platforms[key].height && objects.block.y + objects.block.height > objects.platforms[key].y) {
             objects.block.ax = 0;
             objects.block.vx = 0;
             objects.block.x = objects.platforms[key].x + objects.platforms[key].width;
         }
 
-        if (objects.block.y <= objects.platforms[key].height && objects.block.y + objects.block.height >= objects.platforms[key].height) {
+        if (objects.block.y <= objects.platforms[key].y + objects.platforms[key].height && objects.block.y + objects.block.height >= objects.platforms[key].y + objects.platforms[key].height && objects.block.x < objects.platforms[key].x + objects.platforms[key].width && objects.block.x + objects.block.width > objects.platforms[key].x) {
             objects.block.ay = 0;
             objects.block.vy = 0;
-            objects.block.y = objects.platforms[key].height;
+            objects.block.y = objects.platforms[key].y + objects.platforms[key].height;
             jumping = false;
         } else {
             objects.block.ay = -g;
         }
     }
 
-
-
     if (inputs.left && inputs.right) {
         objects.block.vx = 0;
+        objects.block.ax = 0;
     } else if (inputs.left) {
         objects.block.ax = -xAcceleration;
+        console.log("Left");
     } else if (inputs.right) {
         objects.block.ax = xAcceleration;
+        console.log("Right");
+    }
+
+    if (Math.abs(objects.block.vx) > 5) {
+        objects.block.ax = 0;
     }
 
     if (!inputs.left && !inputs.right) {
@@ -108,7 +96,7 @@ setInterval(function () {
         }
     }
 
-    if (Math.abs(objects.block.vx) < 0.5 && !inputs.right && !inputs.left) {
+    if (Math.abs(objects.block.vx) < 0.2 && !inputs.right && !inputs.left) {
         objects.block.ax = 0;
         objects.block.vx = 0;
     }
@@ -118,9 +106,12 @@ setInterval(function () {
     objects.block.x += objects.block.vx;
     objects.block.y += objects.block.vy;
 
-    objects.platforms.ground.draw();
     objects.block.draw();
-    objects.platforms.step.draw();
+
+    for (const [key, value] of Object.entries(objects.platforms)) {
+        objects.platforms[key].draw();
+    }
+    
 }, 1000 / 60);
 
 onkeydown = function (event) {
@@ -134,7 +125,7 @@ onkeydown = function (event) {
     }
 
     if (event.key === "ArrowUp" && !jumping) {
-        objects.block.y += 1;
+        objects.block.y += 5;
         objects.block.vy += jumpPower;
         jumping = true;
     }
